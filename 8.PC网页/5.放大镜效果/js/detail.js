18292a8921d6e60595688cd0b5bbb9db93ac8b7e
ZhuoranTian <849597043@qq.com>
2021-11-21 17:46:32 +0800

    切图

--- 8.PC网页/5.放大镜效果/js/detail.js
+++ 8.PC网页/5.放大镜效果/js/detail.js
@@ -50,3 +50,75 @@
 //     })
 
 // })
+
+// ①整个案例可以分为三个功能模块
+// ②鼠标经过小图片盒子， 黄色的遮挡层 和 大图片盒子显示，离开隐藏2个盒子功能
+// ③黄色的遮挡层跟随鼠标功能。
+// ④移动黄色遮挡层，大图片跟随移动功能。
+
+// 注意第一行要写
+window.addEventListener('load', function () {
+    let preview_img = document.querySelector('.preview_img');
+    let mask = this.document.querySelector('.mask');
+    let big = document.querySelector('.big');
+    // 1. 当我们鼠标经过 preview_img 就显示和隐藏 mask 遮挡层 和 big 大盒子
+    preview_img.addEventListener('mouseover', function () {
+        mask.style.display = 'block';
+        big.style.display = 'block';
+    })
+    preview_img.addEventListener('mouseout', function () {
+        mask.style.display = 'none';
+        big.style.display = 'none';
+    })
+    // 2. 鼠标移动的时候，让黄色的盒子跟着鼠标来走
+    // 在小盒子内移动
+
+    preview_img.addEventListener('mousemove', function (e) {
+        // (1). 先计算出鼠标在盒子内的坐标
+        let x = e.pageX - this.offsetLeft;
+        let y = e.pageY - this.offsetTop;
+        // console.log(x, y);
+        // 减去盒子高度 300的一半 是 150 就是我们mask 的最终 left 和top值了
+
+        let maskX = x - mask.offsetWidth / 2;
+        let maskY = y - mask.offsetHeight / 2;
+
+        // 遮挡层的最大移动距离
+        var maskMax = preview_img.offsetWidth - mask.offsetWidth;
+        // 如果x 坐标小于了0 就让他停在0 的位置
+        if (maskX <= 0) {
+            maskX = 0;
+        }
+        else if (maskX >= preview_img.offsetWidth - mask.offsetWidth) {
+            maskX = 100;
+
+        } else {
+            mask.style.left = maskX + 'px';
+        }
+
+        if (maskY <= 0) {
+            maskY = 0;
+        }
+        else if (maskY >= preview_img.offsetHeight - mask.offsetHeight) {
+            maskY = 100;
+
+        } else {
+            mask.style.top = maskY + 'px';
+        }
+        // mask.style.top = maskY + 'px';
+
+        // 3. 大图片的移动距离 = 遮挡层移动距离 * 大图片最大移动距离 / 遮挡层的最大移动距离
+        let bigIMg = document.querySelector('.bigImg');
+
+        // 大图片最大移动距离
+        let bigMax = bigIMg.offsetWidth - big.offsetWidth;
+
+        let bigX = maskX * bigMax / maskMax;
+        let bigY = maskY * bigMax / maskMax;
+
+        bigIMg.style.left = -bigX + 'px';
+        bigIMg.style.top = -bigY + 'px';
+
+    })
+
+})
