be68fa1ce474b97e5b45ae05ef58674ee18e0e5b
ZhuoranTian <849597043@qq.com>
2021-12-01 15:41:18 +0800

    切图

--- 10.jQuery/23.购物车案例/js/car.js
+++ 10.jQuery/23.购物车案例/js/car.js
@@ -5,7 +5,14 @@ $(function () {
     $(".checkall").change(function () {
         // console.log($(this).prop("checked"));
         $(".j-checkbox, .checkall").prop("checked", $(this).prop("checked"));
-    })
+        if ($(this).prop("checked")) {
+            // 让所有的商品添加 check-cart-item 类名
+            $(".cart-item").addClass("check-cart-item");
+        } else {
+            // check-cart-item 移除
+            $(".cart-item").removeClass("check-cart-item");
+        }
+    });
     // 2. 如果小复选框被选中的个数等于3 就应该把全选按钮选上，否则全选按钮不选。
     $(".j-checkbox").change(function () {
         // if(被选中的小的复选框的个数 === 3) {
@@ -20,6 +27,13 @@ $(function () {
         } else {
             $(".checkall").prop("checked", false);
 
+        }
+        if ($(this).prop("checked")) {
+            // 让当前的商品添加 check-cart-item 类名
+            $(this).parents(".cart-item").addClass("check-cart-item");
+        } else {
+            // check-cart-item 移除
+            $(this).parents(".cart-item").removeClass("check-cart-item");
         }
     })
     // 3. 增减商品数量模块 首先声明一个变量，当我们点击+号（increment），就让这个值++，然后赋值给文本框。
@@ -49,6 +63,8 @@ $(function () {
         // console.log(p);
         let price = (p * n).toFixed(2);
         $(this).parents(".p-num").siblings(".p-sum").html("￥" + price);
+        getSum();
+
     })
     $(".decrement").click(function () {
         // 得到当前兄弟文本框的值
@@ -62,6 +78,8 @@ $(function () {
         // console.log(p);
         let price = (p * n).toFixed(2);
         $(this).parents(".p-num").siblings(".p-sum").html("￥" + price);
+        getSum();
+
 
 
     })
@@ -73,7 +91,53 @@ $(function () {
         // console.log(p);
         let price = (p * n).toFixed(2);
         $(this).parents(".p-num").siblings(".p-sum").html("￥" + price);
+        getSum();
+
     })
 
+    // 5. 计算总计和总额模块
+    // ①核心思路：把所有文本框里面的值相加就是总计数量。总额同理
+    // ②文本框里面的值不相同，如果想要相加需要用到each遍历。声明一个变量，相加即可
+    // ③点击+号-号，会改变总计和总额，如果用户修改了文本框里面的值同样会改变总计和总额
+    // ④因此可以封装一个函数求总计和总额的， 以上2个操作调用这个函数即可。
+    // ⑤注意1： 总计是文本框里面的值相加用 val() 总额是普通元素的内容用text()
+    // ⑥要注意普通元素里面的内容要去掉￥并且转换为数字型才能相加
+
+
+    const getSum = function () {
+        let count = 0; // 计算总件数 
+        let money = 0; // 计算总价钱
+        $(".itxt").each(function (i, ele) {
+            count += parseInt($(ele).val());
+        })
+        $(".amount-sum em").text(count);
+        $(".p-sum").each(function (i, ele) {
+            // 转为浮点型
+            money += parseFloat($(ele).text().substr(1));
+        })
+        $(".price-sum em").text("￥" + money.toFixed(2));
+
+    }
+    // 首先调用一次
+    getSum();
+    // 6. 删除商品模块
+    // (1) 商品后面的删除按钮
+    $(".p-action a").click(function () {
+        // 删除的是当前的商品 
+        $(this).parents(".cart-item").remove();
+        getSum();
+    })
+    // (2) 删除选中的商品
+
+    $(".remove-batch").click(function () {
+        // 删除的是小的复选框选中的商品
+        $(".j-checkbox:checked").parents(".cart-item").remove();
+        getSum();
+    })
+    // (3) 清空购物车 删除全部商品
+    $(".clear-all").click(function () {
+        $(".cart-item").remove();
+        getSum();
+    })
 
 })
