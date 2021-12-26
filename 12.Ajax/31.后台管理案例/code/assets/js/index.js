835a05b125687ffa79ae158d6701302b36036ade
ZhuoranTian <849597043@qq.com>
2021-12-26 22:18:14 +0800

    切图

--- 12.Ajax/31.后台管理案例/code/assets/js/index.js
+++ 12.Ajax/31.后台管理案例/code/assets/js/index.js
@@ -29,7 +29,7 @@ function getUserInfo() {
       if (res.status !== 0) {
         return layui.layer.msg('获取用户信息失败！')
       }
-      console.log(res);
+      // console.log(res);
       // 调用 renderAvatar 渲染用户的头像
       renderAvatar(res.data)
 
