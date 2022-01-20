d0d69e57dde0798dcbb6723a5644b89ceb446833
ZhuoranTian <849597043@qq.com>
2022-01-20 17:35:07 +0800

    test

--- 13.Node.js/16.使用moment对时间进行格式化.js
+++ 13.Node.js/16.使用moment对时间进行格式化.js
@@ -2,5 +2,6 @@
 // 注意：导入的名称，就是装包时候的名称
 const moment = require('moment')
 
-const dt = moment().format('YYYY-MM-DD HH:mm:ss')
+const dt = moment().format('YYYY-M-DD HH:mm:ss')
+// MM表示补零，M表示不补零
 console.log(dt)
